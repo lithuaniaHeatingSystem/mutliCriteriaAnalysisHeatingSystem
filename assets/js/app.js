f6d555df2765e9e0b276a05db3e5eb626aba6dff
LeoBergerot <leo.bergerot@gmail.com>
2020-02-13 14:01:12 +0200

    config assets

---
+++ assets/js/app.js
@@ -0,0 +1,5 @@
+import '../css/app.css';
+const $ = require('jquery');
+// this "modifies" the jquery module: adding behavior to it
+// the bootstrap module doesn't export/return anything
+require('bootstrap');
