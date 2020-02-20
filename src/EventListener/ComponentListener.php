c4a4d8caf9adadc40c219f9a77a500cedcc0904d
LeoBergerot <leo.bergerot@gmail.com>
2020-02-20 20:15:18 +0200

    add listener on component for create component criteria according to type

---
+++ src/EventListener/ComponentListener.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\EventListener;
+
+use App\Entity\Component;
+use App\Entity\ComponentCriteria;
+use App\Entity\CriteriaType;
+use Doctrine\Persistence\Event\LifecycleEventArgs;
+
+class ComponentListener
+{
+    /**
+     * Add all component criteria according to the new component type
+     *
+     * @param LifecycleEventArgs $args
+     */
+    public function postPersist(LifecycleEventArgs $args)
+    {
+        /** @var CriteriaType $entity */
+        $entity = $args->getObject();
+
+        if (!$entity instanceof Component) {
+            return;
+        }
+        $entityManager = $args->getObjectManager();
+        $criteriaTypes = $entityManager->getRepository(CriteriaType::class)->findBy(['type' => $entity->getType()]);
+        foreach ($criteriaTypes as $type) {
+            if (!$entity->getComponentCriterias()->contains($type)) {
+                $entity->addComponentCriteria((new ComponentCriteria())->setCriteria($type->getCriteria())->setValue(1));
+            }
+            $entityManager->persist($entity);
+        }
+        $entityManager->flush();
+    }
+}
